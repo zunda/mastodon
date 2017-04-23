bc1dac3ff0248e95d10c50f40a961a4771ec89f9
zunda <zundan@gmail.com>
2017-04-23 11:35:03 -1000

    Obtain info from DATABASE_URL

--- .profile.d/dataurl.sh
+++ .profile.d/dataurl.sh
@@ -1,3 +1,10 @@
+# Obtain info from DATABASE_URL
+export DB_USER=`echo $DATABASE_URL | cut -d: -f2 | sed 's/\/\///'`
+export DB_PASS=`echo $DATABASE_URL | cut -d: -f3 | cut -d@ -f1`
+export DB_NAME=`echo $DATABASE_URL | cut -d/ -f4`
+export DB_HOST=`echo $DATABASE_URL | cut -d: -f3 | cut -d@ -f2`
+export DB_PORT=`echo $DATABASE_URL | cut -d: -f4 | cut -d/ -f1`
+
 # Obtain info from REDIS_URL
 export REDIS_HOST=`echo $REDIS_URL | cut -d: -f3 | cut -d@ -f2`
 export REDIS_PORT=`echo $REDIS_URL | cut -d: -f4`
