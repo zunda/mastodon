c03c373c6d1271445d72aa5ce7d266a19c452e39
zunda <zundan@gmail.com>
2017-05-02 17:35:56 -1000

    Avoid overwriting split database and redis split vars

--- .profile.d/mastodon-nodejs-dataurl.sh
+++ .profile.d/mastodon-nodejs-dataurl.sh
@@ -1,13 +1,17 @@
 # Obtain info from DATABASE_URL
-set -- `echo $DATABASE_URL | sed 's/[:/@]/ /g'`
-export DB_USER=$2
-export DB_PASS=$3
-export DB_NAME=$6
-export DB_HOST=$4
-export DB_PORT=$5
+if [ -n "$DATABASE_URL" ]; then
+	set -- `echo $DATABASE_URL | sed 's/[:/@]/ /g'`
+	export DB_USER=$2
+	export DB_PASS=$3
+	export DB_NAME=$6
+	export DB_HOST=$4
+	export DB_PORT=$5
+fi
 
 # Obtain info from REDIS_URL
-set -- `echo $REDIS_URL | sed 's/[:/@]/ /g'`
-export REDIS_HOST=$4
-export REDIS_PORT=$5
-export REDIS_PASSWORD=$3
+if [ -n "$REDIS_URL" ]; then
+	set -- `echo $REDIS_URL | sed 's/[:/@]/ /g'`
+	export REDIS_HOST=$4
+	export REDIS_PORT=$5
+	export REDIS_PASSWORD=$3
+fi
