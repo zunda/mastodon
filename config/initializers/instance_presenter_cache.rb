ac946d42490448394b3bbefda31e7f79dc787663
zunda <zundan@gmail.com>
2017-09-16 10:53:14 -1000

    Adjust max ages of cache

--- config/initializers/instance_presenter_cache.rb
+++ config/initializers/instance_presenter_cache.rb
@@ -1,21 +1,19 @@
 # frozen_string_literal: true
 
 class InstancePresenter
-  MAX_CACHE_AGE = 36.hours
-
   def user_count
-    Rails.cache.fetch('user_count', expires_in: MAX_CACHE_AGE) { User.confirmed.count }
+    Rails.cache.fetch('user_count', expires_in: 37.hours) { User.confirmed.count }
   end
 
   def status_count
-    Rails.cache.fetch('local_status_count', expires_in: MAX_CACHE_AGE) { Status.local.count }
+    Rails.cache.fetch('local_status_count', expires_in: 11.hours) { Status.local.count }
   end
 
   def domain_count
-    Rails.cache.fetch('distinct_domain_count', expires_in: MAX_CACHE_AGE) { Account.distinct.count(:domain) }
+    Rails.cache.fetch('distinct_domain_count', expires_in: 17.hours) { Account.distinct.count(:domain) }
   end
 
   def thumbnail
-    @thumbnail ||= Rails.cache.fetch('site_uploads/thumbnail', expires_in: MAX_CACHE_AGE) { SiteUpload.find_by(var: 'thumbnail') }
+    @thumbnail ||= Rails.cache.fetch('site_uploads/thumbnail', expires_in: 21.hours) { SiteUpload.find_by(var: 'thumbnail') }
   end
 end
