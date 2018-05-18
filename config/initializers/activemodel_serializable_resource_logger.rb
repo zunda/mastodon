06deafbeb28eb8167ee750c9f72c260c262b233d
zunda <zundan@gmail.com>
2018-05-17 21:23:34 -1000

    Log cache

--- config/initializers/activemodel_serializable_resource_logger.rb
+++
@@ -1,9 +0,0 @@
-module ActiveModel
-  class SerializableResource
-    alias :orig_as_json :as_json
-    def as_json(*args)
-      p self
-      orig_as_json(*args)
-    end
-  end
-end
---
+++ config/initializers/cache.rb
@@ -0,0 +1,1 @@
+Rails.cache.logger = Rails.logger
