4896a016fbe71c31c32f0793053a1e6b871c5ac8
hieki <ksakahieki@gmail.com>
2018-11-05 18:46:52 +0900

    Exclude `OpenSSL::SSL::SSLError` exception

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,4 +1,6 @@
 Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.environments = %w(staging production)
+  config.excluded_exceptions += ['OpenSSL::SSL::SSLError']
 end
+
