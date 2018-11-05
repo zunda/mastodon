b43791956efb4fdab5c18ec5164c2707ba38d8ce
Tomoya Chiba <tomo.asleep@gmail.com>
2018-11-05 18:59:02 +0900

    Add frequent exceptions to excluded exceptions
    Co-Authored-By: hieki <tga1751@gmail.com>

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,6 @@
 Raven.configure do |config|
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.environments = %w(staging production)
-  config.excluded_exceptions += ['OpenSSL::SSL::SSLError']
+  config.excluded_exceptions += ['OpenSSL::SSL::SSLError', 'Mastodon::UnexpectedResponseError', 'HTTP::TimeoutError', 'HTTP::ConnectionError']
 end
 
