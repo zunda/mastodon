51efc84684ffe7fe2c817299eeb03fbca6438bda
zunda <zundan@gmail.com>
2018-12-10 21:26:22 -1000

    Add input box for keybase username

--- app/javascript/mastodon/features/compose/containers/pubkeys_container.js
+++ app/javascript/mastodon/features/compose/containers/pubkeys_container.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Button from '../../../components/button';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { injectIntl, defineMessages } from 'react-intl';
 
-const mapStateToProps = (state, { intl }) => ({
+const messages = defineMessages({
+  pubkeys_placeholder: { id: 'pubkeys_list.placeholder', defaultMessage: 'Keybase username for recpient' },
+  pubkeys_add: { id: 'pubkeys_list.add', defaultMessage: 'Add' },
+});
+
+const mapStateToProps = (state) => ({
   encrypt: state.getIn(['compose', 'encrypt']),
 });
 
@@ -12,6 +18,24 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class PubkeysContainer extends React.PureComponent {
+  state = {
+    keybaseUsername: ''
+  };
+
+  handleChange = (e) => {
+    this.setState({ keybaseUsername: e.target.value});
+  }
+
+  handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      this.handleSubmit(e);
+    }
+  }
+
+  handleSubmit = (e) => {
+    console.log(this.state.keybaseUsername);
+    this.setState({ keybaseUsername: ''});
+  }
 
   static propTypes = {
     encrypt: PropTypes.bool.isRequired,
@@ -21,7 +45,10 @@ class PubkeysContainer extends React.PureComponent {
   render () {
     return (
       <div className={`pubkeys-list ${this.props.encrypt ? 'pubkeys-list--visible' : ''}`}>
-        <p>Pubkeys of recipients</p>
+        <label>
+          <span style={{ display: 'none' }}>{this.props.intl.formatMessage(messages.pubkeys_placeholder)}</span>
+          <input placeholder={this.props.intl.formatMessage(messages.pubkeys_placeholder)} value={this.state.keybaseUsername} onChange={this.handleChange} onKeyDown={this.handleKeyDown} type='text' className='pubkeys-list__input' id='pubkeys-input' />
+        </label>
       </div>
     );
   }
