b2c5164a85b6195a61665d3e8a2093942995cdca
zunda <zundan@gmail.com>
2019-11-16 16:17:56 -1000

    Log request body as JSON

--- lib/request_logger.rb
+++ lib/request_logger.rb
@@ -13,39 +13,47 @@ class RequestLogger
     request = ActionDispatch::Request.new env
     begin
       status, _, _ = response = @app.call(env)
-      log(request, status)
+      req_body = request.body.read
+      log(request, req_body, status)
     rescue Exception => exception
-      log(request, status, exception)
+      log(request, req_body, status, exception)
       raise exception
     end
 
     response
   end
 
-  def log(request, status, exception = nil)
+  def log(request, req_body, status, exception = nil)
     h = {
       request: {
         method: request.method,
         fullpath: request.fullpath,
-        header: Hash.new{|h,k| h[k] = Array.new },
-        body: request.body.read,
+        headers: Hash.new{|h,k| h[k] = Array.new },
       },
       response: {
         status: status,
-      }
+      },
     }
 
     request.env.each do |k, v|
       if k.start_with?('HTTP_')
-        h[:request][:header][k] << v
+        h[:request][:headers][k] << v
       end
+    end
+
+    unless req_body.blank?
+      begin
+        req_body = JSON.parse(req_body)
+      rescue JSON::ParserError
+      end
+      h[:request][:body] = req_body
     end
 
     if exception
       h[:exception] = {
-        type: exception&.class&.name,
-        message: exception&.message
-      }
+        type: exception.class.name,
+        message: exception.message,
+      } 
     end
 
     Rails.logger.info(h.to_json)
