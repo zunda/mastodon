dee8a1c8430c65bdeebab0d44cf274738e39a45a
zunda <zundan@gmail.com>
2019-12-01 18:19:16 -1000

    Add more debug output

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,4 +2,10 @@
 
 class Search < ActiveModelSerializers::Model
   attributes :accounts, :statuses, :hashtags
+
+  def initialize
+    Rails.logger.debug("MARKER: #{__FILE__}:#{__LINE__}")
+    super
+    Rails.logger.debug("MARKER: #{__FILE__}:#{__LINE__}")
+  end
 end
