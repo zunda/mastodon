9ea9801e1d447d5aaad70e6ea2dbe278c5608143
zunda <zundan@gmail.com>
2019-12-02 21:58:45 -1000

    Revert "Log response body later"
    This reverts commit 095632b34d041a262b379775fd41c9d61c25b3d7.

--- config/initializers/httplog.rb
+++ config/initializers/httplog.rb
@@ -2,6 +2,5 @@ HttpLog.configure do |config|
   config.logger = Rails.logger
   config.severity = 1
   config.color = { color: :yellow }
-  config.compact_log = true
   config.log_headers = true
 end
