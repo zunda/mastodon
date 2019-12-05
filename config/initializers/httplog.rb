321379e747babe1404979548aa62a3cbbdb4d71a
zunda <zundan@gmail.com>
2019-12-04 18:37:08 -1000

    Stop logging response body by httplog

--- config/initializers/httplog.rb
+++ config/initializers/httplog.rb
@@ -2,5 +2,6 @@ HttpLog.configure do |config|
   config.logger = Rails.logger
   config.severity = 1
   config.color = { color: :yellow }
+  config.compact_log = true
   config.log_headers = true
 end
