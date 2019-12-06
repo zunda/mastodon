b9b56b53687fbcb7016ea349af81952a6de9d7ad
zunda <zundan@gmail.com>
2019-12-05 21:46:33 -1000

    Tweak config for httplog

--- config/initializers/httplog.rb
+++ config/initializers/httplog.rb
@@ -1,5 +1,12 @@
 HttpLog.configure do |config|
   config.logger = Rails.logger
   config.color = { color: :yellow }
-  config.compact_log = true
+
+  config.log_connect   = false
+  config.log_request   = true
+  config.log_headers   = true
+  config.log_data      = false
+  config.log_status    = true
+  config.log_response  = false
+  config.log_benchmark = false
 end
