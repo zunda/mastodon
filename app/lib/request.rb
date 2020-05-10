281f591385e1ceec597df09348a9a7ec96338960
zunda <zundan@gmail.com>
2020-05-10 09:48:02 -1000

    Revert "Nope we need to record the uri with the error"
    This reverts commit 7a40b17c977c9dd60fa526728fd41acb4c34289e.

--- app/lib/request.rb
+++ app/lib/request.rb
@@ -56,11 +56,7 @@ class Request
   end
 
   def perform
-    begin
-      response = http_client.public_send(@verb, @url.to_s, @options.merge(headers: headers))
-    rescue HTTP::ConnectionError, HTTP::TimeoutError => err
-      raise err.class, "#{err.message} on #{@url}"
-    end
+    response = http_client.public_send(@verb, @url.to_s, @options.merge(headers: headers))
 
     begin
       response = response.extend(ClientLimit)
