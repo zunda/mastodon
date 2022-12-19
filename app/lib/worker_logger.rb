6ef415a0865a9ef4e941346e1c7920fce1253e8c
zunda <zundan@gmail.com>
2022-12-18 14:52:56 -1000

    Better log and test delays in worker

--- app/lib/worker_logger.rb
+++ app/lib/worker_logger.rb
@@ -4,6 +4,6 @@ module WorkerLogger
   def log_delay(published, url, at, current_time = Time.current)
     return unless published
     delay = current_time - Time.parse(published).utc
-    logger.info format('source=%s destination=%s measure#delivery.delay=%.0fsec count#%s=1', self.class, url.inspect, delay, at)
+    Rails.logger.info format('source=%s destination=%s measure#delivery.delay=%.0fsec count#%s=1', self.class, url.inspect, delay, at)
   end
 end
--- spec/workers/worker_logger_spec.rb
+++ spec/workers/worker_logger_spec.rb
@@ -2,19 +2,17 @@ require 'rails_helper'
 
 describe WorkerLogger do
   subject { dummy_class.new }
-	let(:dummy_class) { Class.new { include WorkerLogger; def logger; end } }
+	let(:dummy_class) { Class.new { include WorkerLogger } }
 
   before do
-    logger = double(Rails.logger)
-    allow(logger).to receive(:info)
-    allow(subject).to receive(:logger).and_return(logger)
+    allow(Rails.logger).to receive(:info)
   end
 
   describe 'log_delay' do
     it 'logs useful information' do
       subject.log_delay('2017-09-29T14:22:30Z', 'https://example.com/api', 'delivered', Time.utc(2017, 9, 29, 14, 22, 40))
       %w(destination="https://example.com/api" measure#delivery.delay=10sec count#delivered=1).each do |info|
-        expect(subject.logger).to have_received(:info).with(Regexp.new(info))
+        expect(Rails.logger).to have_received(:info).with(Regexp.new(info))
       end
     end
   end
