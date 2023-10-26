2eba1e150dd837359c6dcdd8f3da96765d78e30f
zunda <zundan@gmail.com>
2023-10-25 14:25:27 -1000

    Revert "Fix history handling not properly handling states after update to react-router v5 (#27526)"
    This reverts commit 15182d1e5e595b21c47b48c5d258f30a0251e753.

--- app/javascript/mastodon/components/router.tsx
+++ app/javascript/mastodon/components/router.tsx
@@ -3,11 +3,7 @@ import React from 'react';
 
 import { Router as OriginalRouter } from 'react-router';
 
-import type {
-  LocationDescriptor,
-  LocationDescriptorObject,
-  Path,
-} from 'history';
+import type { LocationDescriptor, Path } from 'history';
 import { createBrowserHistory } from 'history';
 
 import { layoutFromWindow } from 'mastodon/is_mobile';
@@ -24,55 +20,39 @@ const browserHistory = createBrowserHistory<
 const originalPush = browserHistory.push.bind(browserHistory);
 const originalReplace = browserHistory.replace.bind(browserHistory);
 
-function normalizePath(
-  path: HistoryPath,
-  state?: MastodonLocationState,
-): LocationDescriptorObject<MastodonLocationState> {
-  const location = typeof path === 'string' ? { pathname: path } : { ...path };
-
-  if (location.state === undefined && state !== undefined) {
-    location.state = state;
-  } else if (
-    location.state !== undefined &&
-    state !== undefined &&
-    process.env.NODE_ENV === 'development'
-  ) {
-    // eslint-disable-next-line no-console
-    console.log(
-      'You should avoid providing a 2nd state argument to push when the 1st argument is a location-like object that already has state; it is ignored',
-    );
-  }
-
-  if (
-    layoutFromWindow() === 'multi-column' &&
-    !location.pathname?.startsWith('/deck')
-  ) {
-    location.pathname = `/deck${location.pathname}`;
-  }
-
-  return location;
+function extractRealPath(path: HistoryPath) {
+  if (typeof path === 'string') return path;
+  else return path.pathname;
 }
 
 browserHistory.push = (path: HistoryPath, state?: MastodonLocationState) => {
-  const location = normalizePath(path, state);
+  state = state ?? {};
+  state.fromMastodon = true;
 
-  location.state = location.state ?? {};
-  location.state.fromMastodon = true;
+  const realPath = extractRealPath(path);
+  if (!realPath) return;
 
-  originalPush(location);
+  if (layoutFromWindow() === 'multi-column' && !realPath.startsWith('/deck')) {
+    originalPush(`/deck${realPath}`, state);
+  } else {
+    originalPush(path, state);
+  }
 };
 
 browserHistory.replace = (path: HistoryPath, state?: MastodonLocationState) => {
-  const location = normalizePath(path, state);
-
-  if (!location.pathname) return;
-
   if (browserHistory.location.state?.fromMastodon) {
-    location.state = location.state ?? {};
-    location.state.fromMastodon = true;
+    state = state ?? {};
+    state.fromMastodon = true;
   }
 
-  originalReplace(location);
+  const realPath = extractRealPath(path);
+  if (!realPath) return;
+
+  if (layoutFromWindow() === 'multi-column' && !realPath.startsWith('/deck')) {
+    originalReplace(`/deck${realPath}`, state);
+  } else {
+    originalReplace(path, state);
+  }
 };
 
 export const Router: React.FC<PropsWithChildren> = ({ children }) => {
